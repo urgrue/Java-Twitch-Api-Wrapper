75fe6977163cec19f9945dafca460fc7d27f8c29
Matthew Bell <matt.gts@gmail.com>
2015-04-09 21:33:38 -0700

    Updated HttpClient to support most request methods.

--- src/http/HttpClient.java
+++ src/http/HttpClient.java
@@ -1,35 +1,43 @@
 package http;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Map;
 
 public class HttpClient {
 
-    private int connectionTimeout = 20000; // Connection timeout in ms
-    private int dataRetrievalTimeout = 20000; // Data retrieval timeout in ms
+    private int connectionTimeout = 10000; // Connection timeout in ms
+    private int dataRetrievalTimeout = 10000; // Data retrieval timeout in ms
 
     /**
-     * Make a GET Request
-     * @param requestURL Resource URL
-     * @return HttpResponse
+     * Make a HTTP Request
+     * @param resourceUrl Resource URL
+     * @param requestMethod Request Method: GET, POST, PUT, DELETE, etc.
+     * @param data Body data to send with HTTP request
+     * @param headers Headers to include with HTTP request
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
      */
-    public HttpResponse get(String requestURL, Map<String, String> headers) throws IOException {
+    private HttpResponse request(
+                String resourceUrl,
+                String requestMethod,
+                Map<String, String> headers,
+                Map<String, String> data)
+                throws IOException {
 
         HttpResponse response = null;
 
-        URL url = new URL(requestURL);
+        URL url = new URL(resourceUrl);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
         try {
             // Settings
             urlConnection.setConnectTimeout(connectionTimeout);
             urlConnection.setReadTimeout(dataRetrievalTimeout);
-            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestMethod(requestMethod);
+            urlConnection.setUseCaches(false);
             urlConnection.setDoInput(true);
 
             // Headers
@@ -39,14 +47,45 @@ public class HttpClient {
                 }
             }
 
+            // Input data
+            String inputData = "";
+            if (data != null) {
+                StringBuilder sb = new StringBuilder();
+                for (Map.Entry<String, String> datum : data.entrySet()) {
+                    sb.append(datum.getKey());
+                    sb.append("=");
+                    sb.append(URLEncoder.encode(datum.getValue(), "UTF-8"));
+                    sb.append("&");
+                }
+                sb.deleteCharAt(sb.length() - 1); // Remove final '&'
+                inputData = sb.toString();
+
+                urlConnection.setDoOutput(true); // Send data in request body
+                urlConnection.setFixedLengthStreamingMode(inputData.length()); // More efficient since length known
+
+                // Set headers for input content
+                urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                urlConnection.setRequestProperty("charset", "utf-8");
+                urlConnection.setRequestProperty("Content-Length", Integer.toString(inputData.length()));
+            }
+
             urlConnection.connect();
+
+            // Write Input Data
+            if (data != null) {
+                OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
+                writer.write(inputData);
+                writer.flush();
+                writer.close();
+            }
 
             // Response
             int responseCode = urlConnection.getResponseCode();
             String responseMessage = urlConnection.getResponseMessage();
             response = new HttpResponse(responseCode, responseMessage);
 
-            if (responseCode == HttpURLConnection.HTTP_OK) {
+            // 'Successful' response codes will be in interval [200,300)
+            if (responseCode >= 200 && responseCode < 300) {
                 String content = readStream(urlConnection.getInputStream());
                 response.setContent(content);
             } else {
@@ -62,14 +101,108 @@ public class HttpClient {
         return response;
     }
 
-    public HttpResponse get(String requestURL) throws IOException {
-        return get(requestURL, null);
+    /**
+     * Make a HTTP GET Request
+     * @param resourceUrl Resource URL
+     * @param headers Headers to include with HTTP request
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #get(String)
+     */
+    public HttpResponse get(String resourceUrl, Map<String, String> headers) throws IOException {
+        return request(resourceUrl, "GET", headers, null);
+    }
+
+    /**
+     * Make a HTTP GET Request
+     * @param resourceUrl Resource URL
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #get(String, Map)
+     */
+    public HttpResponse get(String resourceUrl) throws IOException {
+        return get(resourceUrl, null);
+    }
+
+    /**
+     * Make a HTTP PUT Request
+     * @param resourceUrl Resource URL
+     * @param headers Headers to include with HTTP request
+     * @param data Body data to send with HTTP request
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #put(String)
+     * @see #put(String, Map)
+     */
+    public HttpResponse put(String resourceUrl, Map<String, String> headers, Map<String, String> data) throws IOException {
+        return request(resourceUrl, "PUT", headers, data);
+    }
+
+    /**
+     * Make a HTTP PUT Request
+     * @param resourceUrl Resource URL
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #put(String, Map)
+     * @see #put(String, Map, Map)
+     */
+    public HttpResponse put(String resourceUrl) throws IOException {
+        return put(resourceUrl, null, null);
+    }
+
+    /**
+     * Make a HTTP PUT Request
+     * @param resourceUrl Resource URL
+     * @param headers Headers to include with HTTP request
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #put(String)
+     * @see #put(String, Map, Map)
+     */
+    public HttpResponse put(String resourceUrl, Map<String, String> headers) throws IOException {
+        return put(resourceUrl, null, headers);
+    }
+
+    /**
+     * Make a HTTP DELETE Request
+     * @param resourceUrl Resource URL
+     * @param headers Headers to include with HTTP request
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #delete(String)
+     */
+    public HttpResponse delete(String resourceUrl, Map<String, String> headers) throws IOException {
+        return request(resourceUrl, "DELETE", headers, null);
+    }
+
+    /**
+     * Make a HTTP DELETE Request
+     * @param resourceUrl Resource URL
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     * @see #put(String, Map)
+     */
+    public HttpResponse delete(String resourceUrl) throws IOException {
+        return delete(resourceUrl, null);
+    }
+
+    /**
+     * Make a HTTP POST Request
+     * @param resourceUrl Resource URL
+     * @param headers Headers to include with HTTP request
+     * @param data Body data to send with HTTP request
+     * @return HttpResponse Response from HTTP request
+     * @throws IOException Error during HTTP request
+     */
+    public HttpResponse post(String resourceUrl, Map<String, String> headers, Map<String, String> data) throws IOException {
+        return request(resourceUrl, "POST", headers, data);
     }
 
     /**
      * Read the input stream and convert to a string
      * @param inputStream InputStream to read
      * @return String representing entire input stream contents
+     * @throws IOException
      */
     private static String readStream(InputStream inputStream) throws IOException {
         if (inputStream == null) {
