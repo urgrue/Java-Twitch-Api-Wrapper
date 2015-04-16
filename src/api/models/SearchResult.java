60a0479b0af36543806cd7d9cf59661762e567ab
Matthew Bell <matt.gts@gmail.com>
2015-04-16 00:06:57 -0700

    Search implemented

--- src/api/models/SearchResult.java
+++ src/api/models/SearchResult.java
@@ -1,16 +1,40 @@
 package api.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchResult<T> {
 
     @JsonProperty("_total") private int total;
     private List<T> results;
+
+    @Override
+    public String toString() {
+        return "SearchResult{" +
+                "total=" + total +
+                ", results=" + results +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SearchResult<?> that = (SearchResult<?>) o;
+
+        if (total != that.total) return false;
+        return !(results != null ? !results.equals(that.results) : that.results != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = total;
+        result = 31 * result + (results != null ? results.hashCode() : 0);
+        return result;
+    }
 
     public int getTotal() {
         return total;
@@ -24,18 +48,7 @@ public class SearchResult<T> {
         return results;
     }
 
-    @JsonSetter("channels")
-    public void setChannels(List<T> results) {
-        this.results = results;
-    }
-
-    @JsonSetter("streams")
-    public void setStreams(List<T> results) {
-        this.results = results;
-    }
-
-    @JsonSetter("games")
-    public void setGames(List<T> results) {
+    public void setResults(List<T> results) {
         this.results = results;
     }
 }
--- src/api/resources/SearchResource.java
+++ src/api/resources/SearchResource.java
@@ -1,9 +1,10 @@
 package api.resources;
 
 import api.TwitchResponse;
-import api.models.Channel;
-import api.models.SearchResult;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import api.models.*;
+import http.HttpResponse;
+
+import java.util.List;
 
 public class SearchResource extends TwitchResource {
 
@@ -15,8 +16,84 @@ public class SearchResource extends TwitchResource {
         String url = String.format("%s/search/channels?q=%s&limit=%s&offset=%s",
                 getBaseUrl(), query, limit, offset);
 
-        throw new NotImplementedException();
+        TwitchResponse<SearchResultContainer> c =
+                requestGet(url, HttpResponse.HTTP_OK, SearchResultContainer.class);
 
-        //return requestGet(url, HttpResponse.HTTP_OK, SearchResult.class);
+        // Extract relevant results
+        SearchResult<Channel> r = new SearchResult<Channel>();
+        if (c.getObject() != null) {
+            r.setTotal(c.getObject().getTotal());
+            r.setResults(c.getObject().getChannels());
+        }
+
+        // Create object with search result rather than the container class
+        TwitchResponse<SearchResult<Channel>> response = new TwitchResponse<SearchResult<Channel>>(
+                c.getStatusCode(),
+                c.getStatusText(),
+                c.getErrorMessage());
+
+        if (!c.hasError()) {
+            response.setObject(r);
+        }
+
+        return response;
+    }
+
+    public TwitchResponse<SearchResult<Stream>> streams(String query, int limit, int offset) {
+        String url = String.format("%s/search/streams?q=%s&limit=%s&offset=%s",
+                getBaseUrl(), query, limit, offset);
+
+        TwitchResponse<SearchResultContainer> c =
+                requestGet(url, HttpResponse.HTTP_OK, SearchResultContainer.class);
+
+        // Extract relevant results
+        SearchResult<Stream> r = new SearchResult<Stream>();
+        if (c.getObject() != null) {
+            r.setTotal(c.getObject().getTotal());
+            r.setResults(c.getObject().getStreams());
+        }
+
+        // Create object with search result rather than the container class
+        TwitchResponse<SearchResult<Stream>> response = new TwitchResponse<SearchResult<Stream>>(
+                c.getStatusCode(),
+                c.getStatusText(),
+                c.getErrorMessage());
+
+        if (!c.hasError()) {
+            response.setObject(r);
+        }
+
+        return response;
+    }
+
+    public TwitchResponse<SearchResult<Game>> games(String query, boolean live) {
+        String url = String.format("%s/search/games?q=%s&type=%s&live=%s",
+                getBaseUrl(), query, "suggest", live);
+
+        TwitchResponse<SearchResultContainer> c =
+                requestGet(url, HttpResponse.HTTP_OK, SearchResultContainer.class);
+
+        // Extract relevant results
+        SearchResult<Game> r = new SearchResult<Game>();
+        if (c.getObject() != null) {
+            r.setTotal(c.getObject().getTotal());
+            r.setResults(c.getObject().getGames());
+        }
+
+        // Create object with search result rather than the container class
+        TwitchResponse<SearchResult<Game>> response = new TwitchResponse<SearchResult<Game>>(
+                c.getStatusCode(),
+                c.getStatusText(),
+                c.getErrorMessage());
+
+        if (!c.hasError()) {
+            response.setObject(r);
+        }
+
+        return response;
+    }
+
+    public TwitchResponse<SearchResult<Game>> games(String query) {
+        return games(query, false);
     }
 }
